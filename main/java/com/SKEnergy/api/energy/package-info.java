e267bc0bded9ba867da44921a2a778bda6913aa1
SwordKorn20 <swordkorn@live.com>
2016-01-20 17:52:13 +0000

    IC-15.0.3 <Adam@Adam-PC Overwrite remote https://github.com/SwordKorn20/SKEnergy to local

--- main/java/com/SKEnergy/api/energy/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * (C) 2016 Adam AKA Sword_Korn
- */
-
-@API(apiVersion = EnergyAPI.VERSION, owner = "EnergyAPI", provides = "EnergyAPI|energy")
-package com.SKEnergy.api.energy;
-
-import net.minecraftforge.fml.common.API;
-import com.SKEnergy.api.EnergyAPI;
